8001b413bbc9d65602648a3c923f85ebada87c26
Hiikun <dark_makai@hotmail.com>
2018-10-23 20:59:46 -0300

    first commit

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Exit : MonoBehaviour 
+{
+    void OnMouseDown()
+    {
+        transform.localScale *= 0.9F;
+    }
+
+    void OnMouseUp()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Quit.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Quit : MonoBehaviour 
+{
+    void OnMouseDown()
+    {
+        transform.localScale *= 1.1F;
+    }
+
+    void OnMouseUp()
+    {
+        Application.LoadLevel(0);
+    }
+}
